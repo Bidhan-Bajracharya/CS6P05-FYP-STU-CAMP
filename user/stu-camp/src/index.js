4eed0b227f3e186f96855a64bb90c1193ada544a
Bidhan Bajracharya <95129959+Bidhan-Bajracharya@users.noreply.github.com>
2022-12-22 18:25:48 +0545

    Added persisting user data in redux slice

--- user/stu-camp/src/index.js
+++ user/stu-camp/src/index.js
@@ -3,27 +3,23 @@ import ReactDOM from "react-dom/client";
 import "./styles/index.css";
 import App from "./App";
 import { AuthProvider } from "./context/AuthProvider";
-import { store } from "./store";
+import { store, persistor } from "./store";
+import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <React.StrictMode>
-  //     <Provider store={store}>
-  //       <AuthProvider>
-  //         <App />
-  //       </AuthProvider>
-  //     </Provider>
-  // </React.StrictMode>
   <React.StrictMode>
     <Router>
       <Provider store={store}>
-        <AuthProvider>
-          <Routes>
-            <Route path="/*" element={<App />} />
-          </Routes>
-        </AuthProvider>
+        <PersistGate loading={null} persistor={persistor}> {/* delay the rendering of our app’s UI until the persisted data is available  */}
+          <AuthProvider>
+            <Routes>
+              <Route path="/*" element={<App />} />
+            </Routes>
+          </AuthProvider>
+        </PersistGate>
       </Provider>
     </Router>
   </React.StrictMode>
--- user/stu-camp/src/store.js
+++ user/stu-camp/src/store.js
@@ -1,20 +1,37 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+import thunk from "redux-thunk";
+
 import navbarReducer from "./features/navbarSlice";
 import sliderReducer from "./features/sliderSlice";
 import stARsReducer from "./features/stARsSlice";
 import homeReducer from "./features/homeSlice";
 import peopleReducer from "./features/peopleSlice";
 import themeReducer from "./features/themeSlice";
-import userReducer from "./features/userSlice"
+import userReducer from "./features/userSlice";
+
+const persistConfig = {
+  key: "root",
+  storage, // local storage
+  whitelist: ['theme', 'user']
+};
+
+const rootReducer = combineReducers({
+  theme: themeReducer,
+  user: userReducer,
+
+  navbar: navbarReducer,
+  slider: sliderReducer,
+  stARs: stARsReducer,
+  home: homeReducer,
+  people: peopleReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    navbar: navbarReducer,
-    slider: sliderReducer,
-    stARs: stARsReducer,
-    home: homeReducer,
-    people: peopleReducer,
-    theme: themeReducer,
-    user: userReducer
-  },
+  reducer: persistedReducer
 });
+
+export const persistor = persistStore(store)
