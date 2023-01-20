45473e80365c82436e1db6e74f6c22b656a8fb53
Bidhan Bajracharya <95129959+Bidhan-Bajracharya@users.noreply.github.com>
2023-01-20 12:51:25 +0545

    Added reset password for students

--- user/stu-camp/src/pages/student/Setting.js
+++ user/stu-camp/src/pages/student/Setting.js
@@ -1,10 +1,76 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import H1 from "../../components/UI/H1";
 import SettingWrapper from "../../components/UI/SettingWrapper";
 import { Avatar } from "antd";
 import { UserOutlined } from "@ant-design/icons";
+import useAxiosPrivate from "../../hooks/useAxiosPrivate";
+import useLogout from "../../hooks/useLogout";
+import { useNavigate } from "react-router-dom";
 
 const Setting = () => {
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmNewPassword, setConfirmNewPassword] = useState("");
+  const [errMsg, setErrMsg] = useState("");
+
+  const axiosPrivate = useAxiosPrivate();
+  const logout = useLogout();
+  const navigate = useNavigate();
+
+  const resetPassword = async (event) => {
+    event.preventDefault();
+
+    // checking done by the regex below
+    // At least one lowercase letter (?=.*[a-z])
+    // At least one uppercase letter (?=.*[A-Z])
+    // At least one number (?=.*\d)
+    // At least one special character (@$!%?&) (?=.[@$!%*?&])
+    // starts and ends with alphanumeric or special characters [A-Za-z\d@$!%*?&]
+
+    try {
+      if (newPassword.length < 8) {
+        throw new Error("Password must be at least 8 characters long");
+      } else if (
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]*$/.test(newPassword)
+      ) {
+        throw new Error(
+          "Password must contain at least 1 lowercase letter, 1 uppercase letter, 1 number and 1 special character"
+        );
+      } else if (/\s/.test(newPassword)) {
+        throw new Error("Password should not contain any spaces or tabs");
+      }
+
+      if (newPassword !== confirmNewPassword) {
+        const error = new Error("Please enter the same password.");
+        throw error;
+      }
+
+      await axiosPrivate.patch(
+        "/users/reset-password",
+        JSON.stringify({ newPassword })
+      );
+
+      // resetting the fields
+      setNewPassword("");
+      setConfirmNewPassword("");
+    } catch (error) {
+      // check if 'error.response' exists
+      if (error.response && error.response.status === 400) {
+        setErrMsg(error.response.data.msg);
+      } else {
+        setErrMsg(error.message);
+      }
+    }
+  };
+
+  useEffect(() => {
+    setErrMsg(""); // empty out any error if the user changes the passwords
+  }, [newPassword, confirmNewPassword]);
+
+  const signout = async () => {
+    await logout();
+    navigate("/login");
+  };
+
   return (
     <>
       <SettingWrapper>
@@ -43,40 +109,51 @@ const Setting = () => {
 
         <H1>Reset Password</H1>
 
-        <div className="flex flex-col ml-7">
-          <input
-            type="text"
-            id="new_password"
-            autoComplete="off"
-            className="w-60 h-9 rounded-3xl align-baseline p-3 mb-4 border-2 border-[#FFA500] focus:outline-[#FFA500] dark:bg-sg dark:text-white"
-            placeholder="New password..."
-          />
+        <form onSubmit={resetPassword} autoComplete="off">
+          <div className="flex flex-col ml-7">
+            <input
+              type="password"
+              id="new_password"
+              autoComplete="off"
+              required
+              value={newPassword}
+              className="w-60 h-9 rounded-3xl align-baseline p-3 mb-4 border-2 border-[#FFA500] focus:outline-[#FFA500] dark:bg-sg dark:text-white"
+              placeholder="New password..."
+              onChange={(e) => setNewPassword(e.target.value)}
+            />
 
-          <input
-            type="text"
-            id="re_password"
-            autoComplete="off"
-            className="w-60 h-9 rounded-3xl align-baseline p-3 mb-4 border-2 border-[#FFA500] focus:outline-[#FFA500] dark:bg-sg dark:text-white"
-            placeholder="Re-write password..."
-          />
+            <input
+              type="password"
+              id="re_password"
+              required
+              value={confirmNewPassword}
+              autoComplete="off"
+              className="w-60 h-9 rounded-3xl align-baseline p-3 mb-4 border-2 border-[#FFA500] focus:outline-[#FFA500] dark:bg-sg dark:text-white"
+              placeholder="Re-write password..."
+              onChange={(e) => setConfirmNewPassword(e.target.value)}
+            />
 
-          <button
-            type="button"
-            className="bg-[#ED820E] rounded-lg h-12 p-2 text-white w-32 hover:bg-[#FC6A03]"
-          >
-            Reset
-          </button>
+            {errMsg && <p className="text-red-600">{errMsg}</p>}
 
-          <p className="italic text-[#808080] text-sm font-light mt-3">
-            Passwords of minimum length 8 is recommended with special characters
-            and numbers.
-          </p>
-        </div>
+            <button
+              type="submit"
+              className="bg-[#ED820E] rounded-lg h-12 p-2 text-white w-32 hover:bg-[#FC6A03]"
+            >
+              Reset
+            </button>
+
+            <p className="italic text-[#808080] text-sm font-light mt-3">
+              Passwords of minimum length 8 is recommended with special
+              characters and numbers.
+            </p>
+          </div>
+        </form>
 
         <H1>Sign-out</H1>
         <button
           type="button"
-          className="bg-[#ED820E] rounded-lg h-12 p-2 text-white w-32 hover:bg-[#FC6A03] ml-7"
+          className="bg-[#ED820E] rounded-lg h-12 p-2 text-white w-32 hover:bg-[#FC6A03] ml-7 lg:mb-2"
+          onClick={() => signout()}
         >
           Sign out
         </button>
